538f374ca7f0825fcde7ee8bcd58058a67f4a3bc
jtechmind <jtechmind@gmail.com>
2025-01-02 23:06:59 +0530

    feat: Create Book entity class with fields and constructors
    - Added Book entity class with fields: id, 	itle, uthor, genre, quantity
    - Included @Entity, @Id, and @GeneratedValue annotations for JPA mapping
    - Implemented no-argument constructor, all-argument constructor, getters, setters, and 	oString() method
    - Ensured proper mapping for persistence in database

---
+++ src/main/java/com/jtechmind/librarymanagement/books/models/Book.java
@@ -0,0 +1,85 @@
+package com.jtechmind.librarymanagement.books.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+//@Getter
+//@Setter
+//@ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String genre;
+    private int quantity;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String author, String genre, int quantity) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
