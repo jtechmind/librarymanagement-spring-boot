d2c29dbef2e401873261eba7f27347f0dd5cf4da
jtechmind <jtechmind@gmail.com>
2025-01-04 21:23:28 +0530

    refactor: Update BookRepository to improve API functionality - Added new methods findByAuthor() and findByTitle()

--- src/main/java/com/jtechmind/librarymanagement/books/repositories/BookRepository.java
+++ src/main/java/com/jtechmind/librarymanagement/books/repositories/BookRepository.java
@@ -4,6 +4,10 @@ import com.jtechmind.librarymanagement.books.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthor(String author);
+    List<Book> findByTitle(String title);
 }
