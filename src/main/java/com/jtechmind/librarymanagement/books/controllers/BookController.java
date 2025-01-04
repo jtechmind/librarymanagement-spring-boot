2e8fa94d41b155b11625b2dc0b168b48aedc7e9a
jtechmind <jtechmind@gmail.com>
2025-01-04 21:20:50 +0530

    refactor: Update BookController to improve API functionality - Added new endpoints to support editional book-related operations - Methods retrieveAllBooks(), getBookByAuthorName(), getBookByTitle(), updateExistingBook(), deleteBook() are added.

--- src/main/java/com/jtechmind/librarymanagement/books/controllers/BookController.java
+++ src/main/java/com/jtechmind/librarymanagement/books/controllers/BookController.java
@@ -4,19 +4,15 @@ import com.jtechmind.librarymanagement.books.models.Book;
 import com.jtechmind.librarymanagement.books.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
     @Autowired
     private BookService bookService;
-
-    @GetMapping("/")
-    public String welcome(){
-        return "Welcome to the books api";
-    }
 
     // Save new book
     @PostMapping("/")
@@ -28,5 +24,33 @@ public class BookController {
     @GetMapping("/{id}")
     public ResponseEntity<Book> getById(@PathVariable Long id){
         return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
+    // Get All Books
+    @GetMapping
+    public ResponseEntity<List<Book>> retrieveAllBooks(){
+        return ResponseEntity.ok(bookService.getAllBook());
+    }
+
+    // Get All Books Of An Author
+    @GetMapping("/author/{author}")
+    public ResponseEntity<List<Book>> getBookByAuthorName(@PathVariable String author){
+        return ResponseEntity.ok(bookService.findBooksByAuthor(author));
+    }
+    // Get Book By Title
+    @GetMapping("/search")
+    public ResponseEntity<List<Book>> getBookByTitle(@PathVariable String title){
+        return ResponseEntity.ok(bookService.findBooksByTitle(title));
+    }
+
+    // Update Existing Book
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateExistingBook(@PathVariable Long id, @RequestBody Book book){
+        return ResponseEntity.ok(bookService.updateBook(id,book));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
     }
 }
