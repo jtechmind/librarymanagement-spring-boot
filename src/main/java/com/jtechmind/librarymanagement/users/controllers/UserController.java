277e6327a2218057a1529a63005db9e9dc9a14da
jtechmind <jtechmind@gmail.com>
2025-01-05 22:05:42 +0530

    Added UserController to handle user-related REST endpoints

---
+++ src/main/java/com/jtechmind/librarymanagement/users/controllers/UserController.java
@@ -0,0 +1,36 @@
+package com.jtechmind.librarymanagement.users.controllers;
+
+import com.jtechmind.librarymanagement.users.models.User;
+import com.jtechmind.librarymanagement.users.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+
+    // CREATE a user
+    @PostMapping("/")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        return ResponseEntity.ok(userService.addUser(user));
+    }
+
+    // GET a user by id
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.findUserById(id));
+    }
+
+    // GET all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        return ResponseEntity.ok(userService.findAllUsers());
+    }
+}
