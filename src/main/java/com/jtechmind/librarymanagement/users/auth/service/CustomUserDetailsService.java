39610b938aa14cb5023dbb7935005ac7c57faa56
jtechmind <jtechmind@gmail.com>
2025-01-06 21:51:50 +0530

    Implemented CustomUsersDetails to provide user-specific authentication details

--- src/main/java/com/jtechmind/librarymanagement/users/auth/service/CustomUserDetailsService.java
+++ src/main/java/com/jtechmind/librarymanagement/users/auth/service/CustomUserDetailsService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 @Service
-public class CustomUserDetailsService11 implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     UserRepository userRepository;
