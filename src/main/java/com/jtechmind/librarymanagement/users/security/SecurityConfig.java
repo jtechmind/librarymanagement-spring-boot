9378e67fa6b9e5c9ee86fc52dbcf06cff2b53b05
jtechmind <jtechmind@gmail.com>
2025-01-08 20:58:40 +0530

    Updated SecurityConfig to permit all requests to /api/v1/users/** and added httpBasic authentication
    - Allowed unauthenticated access to endpoints under /api/v1/users/**
    - Configured HTTP Basic authentication for secured endpoints
    - Ensured other endpoints require authentication

--- src/main/java/com/jtechmind/librarymanagement/users/security/SecurityConfig.java
+++ src/main/java/com/jtechmind/librarymanagement/users/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,8 +25,10 @@ public class SecurityConfig {
    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
        return http.csrf(csrf-> csrf.disable())
                .authorizeHttpRequests((auth)-> auth.requestMatchers("/api/v1/users/auth/**").permitAll()
+                       .requestMatchers("/api/v1/users/**").permitAll()
                        .anyRequest().authenticated())
                .userDetailsService(customUserDetailsService)
+               .httpBasic(Customizer.withDefaults())
                .build();
 
    }
