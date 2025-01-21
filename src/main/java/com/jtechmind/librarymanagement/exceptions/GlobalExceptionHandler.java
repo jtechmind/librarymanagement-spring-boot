3487ec626566d7af1f044f17ec4b293d472e17cb
jtechmind <jtechmind@gmail.com>
2025-01-21 23:37:21 +0530

    feat: Implement global exception handling for better error responses
    - Created BookNotFoundException to handle cases where a book is not found by ID.
    - Implemented GlobalExceptionHandler to handle BookNotFoundException and general exceptions.
    - Improved API responses by returning appropriate HTTP status codes (404 for not found, 500 for internal errors).
    - Added a custom error message instead of the default Whitelabel Error Page.

---
+++ src/main/java/com/jtechmind/librarymanagement/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.jtechmind.librarymanagement.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    public ResponseEntity<String> handleBookNotFoundException(BookNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong: " + ex.getMessage());
+    }
+}
