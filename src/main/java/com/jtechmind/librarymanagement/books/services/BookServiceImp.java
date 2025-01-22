e87cc4b0d5efbb298a381eee858a065ef78f447a
jtechmind <jtechmind@gmail.com>
2025-01-23 00:01:46 +0530

    Enhanced validation handling in createBook() method
    - Integrated manual validation using Validator API.
    - Throws a meaningful exception with detailed constraint violations.
    - Ensured validation occurs before persisting the Book entity.
    - Improved error handling for better debugging and API response.

--- src/main/java/com/jtechmind/librarymanagement/books/services/BookServiceImp.java
+++ src/main/java/com/jtechmind/librarymanagement/books/services/BookServiceImp.java
@@ -3,17 +3,32 @@ package com.jtechmind.librarymanagement.books.services;
 import com.jtechmind.librarymanagement.books.models.Book;
 import com.jtechmind.librarymanagement.books.repositories.BookRepository;
 import com.jtechmind.librarymanagement.exceptions.BookNotFoundException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
+
 @Service
 public class BookServiceImp implements BookService{
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private Validator validator;
 
     @Override
     public Book createBook(Book book) {
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        if(!violations.isEmpty()){
+            String errorMessage = violations.stream()
+                    .map(v -> v.getPropertyPath()+" "+v.getMessage())
+                    .reduce((m1, m2) -> m1 + ","+ m2)
+                    .orElse("Validation Failed");
+            throw new ConstraintViolationException(errorMessage,violations);
+        }
         return bookRepository.save(book);
     }
 
