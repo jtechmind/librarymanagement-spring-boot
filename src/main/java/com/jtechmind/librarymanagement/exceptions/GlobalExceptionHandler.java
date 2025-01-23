63b6c86cf9322c50bedae249e4846cb4d7f01b54
jtechmind <jtechmind@gmail.com>
2025-01-23 23:57:17 +0530

    feat: Add handleValidationException() in GlobalExceptionHandler
    - Added handleValidationException() to handle ConstraintViolationException.
    - Returns a user-friendly error message for validation failures.
    - Enhances global exception handling with meaningful validation messages.

--- src/main/java/com/jtechmind/librarymanagement/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/jtechmind/librarymanagement/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,14 @@
 package com.jtechmind.librarymanagement.exceptions;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,5 +19,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGeneralException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong: " + ex.getMessage());
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleValidationException(ConstraintViolationException ex) {
+        return ex.getConstraintViolations().stream()
+                .map(v -> v.getPropertyPath() + " " + v.getMessage())
+                .collect(Collectors.joining(", "));
     }
 }
