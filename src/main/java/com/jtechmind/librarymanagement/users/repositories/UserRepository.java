f392a1f6cc21207b27b44c2edddb68973614d7e2
jtechmind <jtechmind@gmail.com>
2025-01-25 23:59:12 +0530

    feat: Add methods for checking duplicate email and username in UserRepository
    - Added findByEmail(String email) to retrieve user by email
    - Added existsByEmail(String email) to check for duplicate email
    - Added existsByName(String name) to check for duplicate username
    - Improves validation to prevent duplicate user registration

--- src/main/java/com/jtechmind/librarymanagement/users/repositories/UserRepository.java
+++ src/main/java/com/jtechmind/librarymanagement/users/repositories/UserRepository.java
@@ -9,4 +9,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByName(String name);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByName(String name);
+
 }
