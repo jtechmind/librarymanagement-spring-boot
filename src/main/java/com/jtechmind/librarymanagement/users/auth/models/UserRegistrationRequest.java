21009efba31031f599a2b12fc30eb692d0866628
jtechmind <jtechmind@gmail.com>
2025-01-25 23:50:28 +0530

    feat: Add UserRegistrationRequest DTO with validation
    - Created UserRegistrationRequest DTO
    - Added validation for username, email, and password using Jakarta Validation API
    - Ensured proper constraints for user input during registration

---
+++ src/main/java/com/jtechmind/librarymanagement/users/auth/models/UserRegistrationRequest.java
@@ -0,0 +1,42 @@
+package com.jtechmind.librarymanagement.users.auth.models;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+public class UserRegistrationRequest {
+    @NotBlank(message = "Name is required")
+    @Size(min = 4, max = 30, message = "Name must be between 4 and 30 characters")
+    private String name;
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password at least 6 characters long")
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
